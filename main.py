f5476e060b867147a062981e7d57292ffda81ac8
Ilya Loshkarev <aurgtangfovr@gmail.com>
2022-11-25 18:43:08 +0300

    First Pygame

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-# TEst Git Example
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pygame
+import math
+BG_COLOR = pygame.Color('yellow')
+RECT_COLOR = pygame.Color(128, 16, 16)
+CIR_COLOR = pygame.Color(16, 128, 16)
+LINE_COLOR = pygame.Color(16, 16, 128)
 
+pygame.init()
+size = width, height = 800, 600
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('MY SUPER GAME')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+icon = pygame.Surface((32, 32))
+pygame.draw.circle(icon, BG_COLOR, (16,16), 16)
+pygame.display.set_icon(icon)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+screen.fill(BG_COLOR)
+screen.fill(RECT_COLOR, (400,0, 400, 600))
+
+pygame.draw.rect(screen, RECT_COLOR, (100, 100,  150, 250), border_radius=16)
+pygame.draw.circle(screen, CIR_COLOR, (100, 100), 75, width=8)
+pygame.draw.ellipse(screen, CIR_COLOR, (250, 250,  100, 150))
+pygame.draw.line(screen, LINE_COLOR, (175, 100), (450, 100), width=8)
+
+n = 16
+poly = []
+for i in range(n):
+    x = width // 2 + math.cos(i * 2 * math.pi / n) * 100
+    y = width // 2 + math.sin(i * 2 * math.pi / n) * 100
+    poly.append((x,y))
+
+pygame.draw.lines(screen, LINE_COLOR, True, poly, width=8)
+pygame.draw.polygon(screen, RECT_COLOR, poly)
+
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+
+pygame.quit()
